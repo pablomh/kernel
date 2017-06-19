c6ae169f3e1f7cac45d9581ddbd02d4b5b217f7f
Angelo G. Del Regno <kholk11@gmail.com>
2017-06-19 20:45:47 +0200

    cpuidle: lpm-levels: Compile no-PSCI code only for MSM8916-class
    Only MSM8916-class SoCs do have MSM_PM for no-PSCI.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1279,6 +1279,7 @@ unlock_and_return:
 	return state_id;
 }
 
+#if defined(CONFIG_MSM_PM) && defined(CONFIG_ARCH_MSM8916)
 /**
  * lpm_cpu_hotplug_enter(): Called by dying CPU to terminate in low power mode
  *
@@ -1334,6 +1335,7 @@ void lpm_cpu_hotplug_enter(unsigned int cpu)
 
 	msm_cpu_pm_enter_sleep(mode, false);
 }
+#endif
 
 static bool psci_enter_sleep(struct lpm_cpu *cpu, int idx, bool from_idle)
 {
